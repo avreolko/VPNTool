3390a1b1e76fe64a1f093067dcf2a83b19447898
avreolko <vacherep@gmail.com>
2014-11-18 18:07:09 +0600

    Initial Commit

---
+++ VPNTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VPNTool
+//
+//  Created by Valentin Cherepyanko on 18/11/14.
+//  Copyright (c) 2014 Valentin Cherepyanko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VPNTool/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VPNTool
+//
+//  Created by Valentin Cherepyanko on 18/11/14.
+//  Copyright (c) 2014 Valentin Cherepyanko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
